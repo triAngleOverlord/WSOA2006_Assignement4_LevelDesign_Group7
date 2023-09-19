7bd521e7dac7ac2c350b458aacabcc6c378b160d
triAngleOverlord <2328564@students.wits.ac.za>
2023-09-19 18:35:06 +0200

    basic flying mechanic with broom bar values need tweaking according to level layout

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,25 +7,37 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform pCamera;
     public Transform cameraPOS;
+    private Rigidbody rb;
 
-    public float horiINPUT;
-    public float vertINPUT;
-    public Vector3 moveDirection;
+    private float horiINPUT;
+    private float vertINPUT;
+    private Vector3 moveDirection;
 
-    public Rigidbody rb;
-    public float dragF;
 
-    public float speedRotate;
+    private float speedRotate;
     public float speedMove;
 
-    public float xRotate;
-    public float yRotate;
+    private float xRotate;
+    private float yRotate;
 
-    
+    public float groundDrag;
+    private float playerHeight;
+    public LayerMask theGround;
+    bool grounded;
+
+    public float flightStamina;
+    public float flightForce;
+    public float airDrag;
+    public float flightCost;
+    public Transform broomBar;
+    bool isFlying;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerHeight = 2;
+        speedRotate = 2;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         rb = GetComponent<Rigidbody>();
     }
@@ -39,8 +52,27 @@ public class PlayerMovement : MonoBehaviour
         pCamera.eulerAngles = new Vector3(Mathf.Clamp(yRotate, -90f, 90f), xRotate, 0.0f);
         transform.eulerAngles = new Vector3(0, xRotate, 0.0f);
 
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, theGround);
+        flightStamina = broomBar.GetComponent<Slider>().value;
+
+
         myInput();
-        rb.drag = dragF;
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+            speedMove = 5;
+        }
+        else if (isFlying)
+        {
+            rb.drag = airDrag;
+            speedMove = 5;
+        }
+        else
+        {
+            rb.drag = 0.0f;
+            speedMove = 0.5f;
+        }
+            
     }
 
     private void FixedUpdate()
@@ -53,12 +85,30 @@ public class PlayerMovement : MonoBehaviour
         horiINPUT = Input.GetAxisRaw("Horizontal");
         vertINPUT = Input.GetAxisRaw("Vertical");
 
+        if(Input.GetKey(KeyCode.Space) && flightStamina !=0)
+        {
+            isFlying = true;
+            fly();
+        }
+        else
+            isFlying= false;
+
     }
 
     private void movePlayer()
     {
         moveDirection = transform.forward * vertINPUT + transform.right * horiINPUT;
-        rb.AddForce(moveDirection.normalized * speedMove, ForceMode.Force);
+        rb.AddForce(moveDirection.normalized * speedMove * 10.0f, ForceMode.Impulse);
 
+    }
+
+    private void fly()
+    {
+        rb.AddForce(transform.up * flightForce, ForceMode.Impulse);
+        flightStamina -= flightCost *Time.deltaTime;
+        broomBar.GetComponent<Slider>().value = flightStamina;
+
+        if (flightStamina < 0)
+            flightStamina = 0;
     }
 }
